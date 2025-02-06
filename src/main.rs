b4b68f2386441481359e0e469ea1c6b707ca734a
Shineaki <halmosadam92@gmail.com>
2025-02-06 22:02:43 +0100

    Lightyear example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-// this is where we read the settings file from assets/settings.ron 
-// and create the client or server app depending on the passed CLI arguments.
-mod protocol;
-use bevy::math::Vec2;
-use protocol::*;
+//! This example showcases how to use Lightyear with Bevy, to easily get replication along with prediction/interpolation working.
+//!
+//! There is a lot of setup code, but it's mostly to have the examples work in all possible configurations of transport.
+//! (all transports are supported, as well as running the example in client-and-server or host-server mode)
+//!
+//!
+//! Run with
+//! - `cargo run -- server`
+//! - `cargo run -- client -c 1`
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use crate::settings::get_settings;
+use bevy::prelude::*;
+use lightyear_examples_common::app::{Apps, Cli};
+use lightyear_examples_common::settings::Settings;
+use protocol::ProtocolPlugin;
 
-fn main(){
-    let qwe = Vec2{x: 1, y: 2};
-    let abc = PlayerPosition(qwe);
-    print!("{abc}");
+#[cfg(feature = "client")]
+mod client;
+mod protocol;
+#[cfg(feature = "gui")]
+mod renderer;
+#[cfg(feature = "server")]
+mod server;
+mod settings;
+mod shared;
+
+fn main() {
+    let cli = Cli::default();
+    #[allow(unused_mut)]
+    let mut settings = get_settings();
+    #[cfg(target_family = "wasm")]
+    lightyear_examples_common::settings::modify_digest_on_wasm(&mut settings.client);
+    // build the bevy app (this adds common plugin such as the DefaultPlugins)
+    // and returns the `ClientConfig` and `ServerConfig` so that we can modify them if needed
+    let mut apps = Apps::new(settings, cli, env!("CARGO_PKG_NAME").to_string());
+    // add the `ClientPlugins` and `ServerPlugins` plugin groups
+    apps.add_lightyear_plugins();
+    apps.add_user_shared_plugin(ProtocolPlugin);
+    #[cfg(feature = "client")]
+    apps.add_user_client_plugin(client::ExampleClientPlugin);
+    #[cfg(feature = "server")]
+    apps.add_user_server_plugin(server::ExampleServerPlugin);
+    #[cfg(feature = "gui")]
+    apps.add_user_renderer_plugin(renderer::ExampleRendererPlugin);
+    // run the app
+    apps.run();
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,2 +1,61 @@
-// here we parse the settings.ron file and have helpers to create the ClientConfig 
-// and ServerConfig structs which are all that is required to build a ClientPlugin or a ServerPlugin
+use lightyear::prelude::CompressionConfig;
+use lightyear_examples_common::settings::{
+    ClientSettings, ClientTransports, Conditioner, ServerSettings, ServerTransports, Settings,
+    SharedSettings, WebTransportCertificateSettings,
+};
+use std::net::Ipv4Addr;
+use std::string::ToString;
+
+pub(crate) fn get_settings() -> Settings {
+    Settings {
+        server: ServerSettings {
+            headless: false,
+            inspector: true,
+            conditioner: Some(Conditioner {
+                latency_ms: 200,
+                jitter_ms: 20,
+                packet_loss: 0.05,
+            }),
+            transport: vec![
+                ServerTransports::WebTransport {
+                    local_port: 5000,
+                    certificate: WebTransportCertificateSettings::AutoSelfSigned(vec!["localhost".to_string()]),
+                    // FromFile {
+                    //     cert: "../certificates/ssl-cert-snakeoil.pem".to_string(),
+                    //     key: "../certificates/ssl-cert-snakeoil.key".to_string(),
+                    // },
+                },
+                ServerTransports::Udp { local_port: 5001 },
+                // #[cfg(feature = "websocket")]
+                // ServerTransports::WebSocket { local_port: 5002 },
+                // #[cfg(feature = "steam")]
+                // ServerTransports::Steam {
+                //     app_id: 480,
+                //     server_ip: Ipv4Addr::UNSPECIFIED,
+                //     game_port: 5003,
+                //     query_port: 27016,
+                // },
+            ],
+        },
+        client: ClientSettings {
+            inspector: true,
+            client_id: 0,
+            client_port: 0, // 0 means that the OS will assign a random port
+            server_addr: Ipv4Addr::LOCALHOST,
+            server_port: 5000, // change the port depending on the transport used
+            transport: ClientTransports::WebTransport {
+                #[cfg(target_family = "wasm")]
+                certificate_digest: include_str!("../../certificates/digest.txt").to_string(),
+            },
+            conditioner: None,
+        },
+        shared: SharedSettings {
+            protocol_id: 0,
+            private_key: [
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0,
+            ],
+            compression: CompressionConfig::None,
+        },
+    }
+}
